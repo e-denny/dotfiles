efdb625f6cb6a168001fb0239066c647b7ae43c8
Edgar Denny <edgar1denny@gmail.com>
2026-09-20 16:01:05 -0400

    nvim(mdmath): render markdown image links inline with Kitty
    - open_image_key (gx): open image/attachment links under the cursor
    - toggle_images_key (<leader>ti): toggle automatic in-place image previews
    - lazy-lock: bump mdmath.nvim to c210910 (inline image rendering)

--- nvim/lua/plugins/mdmath.lua
+++ nvim/lua/plugins/mdmath.lua
@@ -14,6 +14,11 @@ return {
       dynamic = true,
       dynamic_scale = 0.7,
       inline_scale = 0.8,
+      -- Open the image/attachment link under the cursor with `gx`
+      -- (Obsidian ![[...]] / [[...svg|...]] or markdown ![...](...)).
+      open_image_key = "gx",
+      -- Toggle automatic in-place image previews on/off.
+      toggle_images_key = "<leader>ti",
     },
   },
 }
